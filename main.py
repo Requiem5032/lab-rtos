668e42ebcb3ead484c10369455b17f070a55507d
Requiem <public_receiver035@pm.me>
2023-05-16 13:47:29 +0700

    lab 2b

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello LAB2")
+import time
+from scheduler import *
+from task1 import *
+from task2 import *
+scheduler = Scheduler()
+scheduler.SCH_Init()
+
+task1 = task1()
+task2 = task2()
+
+scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
+
+while True:
+    scheduler.SCH_Update()
+    scheduler.SCH_Dispatch_Tasks()
+    time.sleep(1)
---
+++ task1.py
@@ -0,0 +1,7 @@
+class task1:
+  def __init__(self):
+    print("Init task 1")
+    return
+
+  def Task1_Run(self):
+    print("Task 1 is activated")
