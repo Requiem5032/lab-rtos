a01008d22691ec11b5289dee2ff709528418c27e
Tuna <vhtuananh020402@gmail.com>
2023-05-30 10:09:20 +0700

    upload

---
+++ 30.05.2023/main.py
@@ -0,0 +1,23 @@
+import time
+
+import scheduler
+from task1 import *
+from task2 import *
+from AItask import *
+
+scheduler = scheduler.Scheduler()
+scheduler.SCH_Init()
+# task1 = Task1()
+# task2 = Task2()
+aitask = AItask()
+
+
+# scheduler.SCH_Add_Task(task1.Task1Run, 1000, 2000)
+# scheduler.SCH_Add_Task(task2.Task2Run, 2000, 4000)
+scheduler.SCH_Add_Task(aitask.aitaskRun, 3000, 5000)
+
+while True:
+    scheduler.SCH_Update()
+    scheduler.SCH_Dispatch_Tasks()
+    time.sleep(1)
+
